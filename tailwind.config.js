d3a8f2b80b12c27e3655a0c1dfa6ff2c4e5558bd
Faysa Ryestitha Etsuko <etsukofr21d@student.unhas.ac.id>
2023-10-17 17:48:17 +0800

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "white": "#FFFFFF",
+        "navy": "#1F3576",
+        "red": "#EB2629",
+        "gray": "#222222"
+      }
+    },
+    fontFamily: {
+      Montserrat: ["Montserrat, sans-serif"],
+      Inter: ["Inter, sans-serif"]
+    },
+  },
+  plugins: [],
+}
+
