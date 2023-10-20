3c0867ffb58f70148f25a7d97de06526024a11e5
NurhalizaSulthan <nurhalizas21d@student.unhas.ac.id>
2023-10-20 15:54:50 +0700

    Dharma dan Footer

--- src/components/Dharma.vue
+++ src/components/Dharma.vue
@@ -1,32 +1,21 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class=" bg-navy text-white mx-auto  p-8">
+    <div>
+      <h6 class="font-bold underline">DHARMA WANITA</h6>
+    </div>
+    <div class="px-14 py-8 flex gap-1 justify-around  mx-auto">
+      <div class="container mx-auto py-2 flex items-center ">
+        <img src="../assets/dharma.png" alt="Dharma Wanita" class="w-100 h-64 rounded-lg">
+      </div>
+      <div class="justify-items-start gap-y-8 mt-9">
+        <div>
+          <p CLASS="text-left text-1xl">Dharma Wanita Association of the Faculty of Engineering, Hasanuddin University, chaired by Mrs. Muralia Isran Ramli, actively carries out various activities such as Ramadan charity, social gatherings on ships, BKMT and DPW Unhas activities.</p>
+        </div>
+        <div class="relative mt-4">
+          <button class=" rounded-full border-4 border-sky-800 h-8 w-28 absolute inset-y-0 left-0  bg-white text-navy px-3  ">Read more</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -41,18 +30,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
