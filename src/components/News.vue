6f9597d0c51b9219ba77439356dfd4eab7ab77a4
Faysa Ryestitha Etsuko <etsukofr21d@student.unhas.ac.id>
2023-10-20 22:11:58 +0800

    update news dan sambutan

--- src/components/News.vue
+++ src/components/News.vue
@@ -4,61 +4,61 @@
             <div class="basis-2/3 bg-navy font-Montserrat text-white font-medium text-center rounded text-3xl py-2">NEWS</div>
             <div class="basis-1/3 bg-navy font-Montserrat text-white font-medium text-center rounded text-3xl py-2">INFORMATION</div>
         </div>
-        <div class="flex flex-row space-x-10 max-h-fit">
-            <div class="basis-2/3 font-Montserrat text-white font-bold text-center rounded-sm relative">
+        <div class="flex flex-row space-x-10">
+            <div class="container basis-2/3 rounded-sm relative max-h-[450px]">
                 <img src="../assets/1.jpeg" class="rounded-md w-full">
-                <span class=" absolute left-6 bottom-32">
+                <span class=" absolute left-6 bottom-28">
                     <img src="../assets/tanggal.png">
                 </span>
-                <span class="absolute left-6 right-24 bottom-11">
-                    <p class="font-Inter font-extrabold text-white text-justify text-shadow-sm shadow-black text-xl">The Faculty of Engineering Hasanuddin University collaborate with the Indonesian Consultants Association or Perkindo by signing a Memorandum of Agreement on Monday, August 28th, 2023 in the CSA Building</p>
+                <span class="absolute left-6 right-24 bottom-4">
+                    <p class="font-Inter font-bold text-white text-justify text-shadow-sm shadow-black text-md">The Faculty of Engineering Hasanuddin University collaborate with the Indonesian Consultants Association or Perkindo by signing a Memorandum of Agreement on Monday, August 28th, 2023 in the CSA Building</p>
                 </span>
-                <span class="absolute right-2 bottom-4">
+                <span class="absolute right-2 bottom-3">
                     <img src="../assets/readmore.png">
                 </span>
             </div>
             <div class="border-2 border-navy basis-1/3 rounded-sm space-y-5 p-5 grid justify-items-center">
                 <div class="space-y-2">
-                    <p class="text-black font-Inter text-left font-light text-xl">THE 3RD EPI INTERNATIONAL CONFERENCE ON SCIENCE AND ENGINEERING 2019</p>
+                    <p class="text-black font-Inter text-left font-light text-sm">THE 3RD EPI INTERNATIONAL CONFERENCE ON SCIENCE AND ENGINEERING 2019</p>
                     <div class="flex items-center space-x-2">
                         <div>
                             <img src="../assets/calendar.png" width="14">
                         </div>
                         <div>
-                            <p class="text-gray2 font-Inter font-extralight text-md">September 23rd, 2019</p>
+                            <p class="text-gray2 font-Inter font-extralight text-xs">September 23rd, 2019</p>
                         </div>
                     </div>
                 </div>
                 <div class="space-y-2">
-                    <p class="text-black font-Inter text-left font-light text-xl">WORKSHOP & SIMPOSIUM NASIONAL GEOMEKANIKA 5 2019</p>
+                    <p class="text-black font-Inter text-left font-light text-sm">WORKSHOP & SIMPOSIUM NASIONAL GEOMEKANIKA 5 2019</p>
                     <div class="flex items-center space-x-2">
                         <div>
                             <img src="../assets/calendar.png" width="14">
                         </div>
                         <div>
-                            <p class="text-gray2 font-Inter font-extralight text-md">August 6th, 2019</p>
+                            <p class="text-gray2 font-Inter font-extralight text-xs">August 6th, 2019</p>
                         </div>
                     </div>
                 </div>
                 <div class="space-y-2">
-                    <p class="text-black font-Inter text-left font-light text-xl">UNHAS STUDENT LEADERSHIP ACADEMY (USLA) 2019</p>
+                    <p class="text-black font-Inter text-left font-light text-sm">UNHAS STUDENT LEADERSHIP ACADEMY (USLA) 2019</p>
                     <div class="flex items-center space-x-2">
                         <div>
                             <img src="../assets/calendar.png" width="14">
                         </div>
                         <div>
-                            <p class="text-gray2 font-Inter font-extralight text-md">July 9th, 2019</p>
+                            <p class="text-gray2 font-Inter font-extralight text-xs">July 9th, 2019</p>
                         </div>
                     </div>
                 </div>
                 <div class="space-y-2">
-                    <p class="text-black font-Inter text-left font-light text-xl">INTERNATIONAL GUEST LECTURE ON JOURNAL ARTICLE</p>
+                    <p class="text-black font-Inter text-left font-light text-sm">INTERNATIONAL GUEST LECTURE ON JOURNAL ARTICLE</p>
                     <div class="flex items-center space-x-2">
                         <div>
                             <img src="../assets/calendar.png" width="14">
                         </div>
                         <div>
-                            <p class="text-gray2 font-Inter font-extralight text-md">Juny 25th, 2019</p>
+                            <p class="text-gray2 font-Inter font-extralight text-xs">Juny 25th, 2019</p>
                         </div>
                     </div>
                 </div>
@@ -70,7 +70,7 @@
             </div>
         </div>
         <div class="flex flex-row justify-between">
-            <div class="space-y-1 w-80">
+            <div class="space-y-1 w-60">
                 <div>
                     <img src="../assets/2.jpg" class="w-full">
                 </div>
@@ -92,13 +92,13 @@
                     </div>
                 </div>
                 <div>
-                    <p class="font-Inter text-black text-xl">Faculty of Engineering Hasanuddin University Celebrates 63rd Anniversary</p>
+                    <p class="font-Inter text-black text-md">Faculty of Engineering Hasanuddin University Celebrates 63rd Anniversary</p>
                 </div>
                 <div>
                     <img src="../assets/readmore.png" width="100">
                 </div>
             </div>
-            <div class="w-80 space-y-1">
+            <div class="w-60 space-y-1">
                 <div>
                     <img src="../assets/3.jpg" class="w-full">
                 </div>
@@ -120,13 +120,13 @@
                     </div>
                 </div>
                 <div>
-                    <p class="font-Inter text-black text-xl">Faculty of Engineering Establishes Collaboration with Electronics Engineering Polytechnic Institute of Surabaya</p>
+                    <p class="font-Inter text-black text-md">Faculty of Engineering Establishes Collaboration with Electronics Engineering Polytechnic Institute of Surabaya</p>
                 </div>
                 <div>
                     <img src="../assets/readmore.png" width="100">
                 </div>
             </div>
-            <div class="w-80 space-y-1">
+            <div class="w-60 space-y-1">
                 <div>
                     <img src="../assets/4.jpg" class="w-full">
                 </div>
@@ -148,13 +148,13 @@
                     </div>
                 </div>
                 <div>
-                    <p class="font-Inter text-black text-xl">LPJK Holds Roadshow to Faculty of Engineering Campus</p>
+                    <p class="font-Inter text-black text-md">LPJK Holds Roadshow to Faculty of Engineering Campus</p>
                 </div>
                 <div>
                     <img src="../assets/readmore.png" width="100">
                 </div>
             </div>
-            <div class="space-y-1 w-80">
+            <div class="space-y-1 w-60">
                 <div>
                     <img src="../assets/5.jpg" class="w-full">
                 </div>
@@ -176,7 +176,7 @@
                     </div>
                 </div>
                 <div>
-                    <p class="font-Inter text-black text-xl">Increasing International Collaboration, Faculty of Engineering, Hasanuddin University Holds ICAST 2023</p>
+                    <p class="font-Inter text-black text-md">Increasing International Collaboration, Faculty of Engineering, Hasanuddin University Holds ICAST 2023</p>
                 </div>
                 <div>
                     <img src="../assets/readmore.png" width="100">
