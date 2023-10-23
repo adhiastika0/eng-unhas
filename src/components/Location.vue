a2c81900dbb9243cf00cdb789690e11e2dd3da6f
Faysa Ryestitha Etsuko <etsukofr21d@student.unhas.ac.id>
2023-10-23 13:51:38 +0800

    update bang

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="hello">
-    <div class="bg-navy h-[450px] flex mx-[100px] p-[20px]">
-      <div class="ml-[30px]">
-        <img class="h-20 w-auto mt-100" src="https://cot.unhas.ac.id/public/homepage/images/img/unhas.png">
-        <input class="text-black mb-[10px] mt-[10px] w-[400px] h-[35px] pl-5" type="text" placeholder="Search">
-        <div class="relative">
-          <button @click="show = !show" class="flex items-center p-2 text-navy bg-white text-left w-[400px] h-[35px]">
-            <span class="w-500 whitespace-no-wrap">Faculty Of Engineering</span>
-            <svg class="w-5 h-5 text-navy dark:text-navy ml-[170px]" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"/></svg>
-          </button>
-          <div v-show="show" class="py-2 mt-2 bg-white rounded-0 shadow-xlw-44 ml-0 text-left" @mouseenter="isDropdownHovered = true" @mouseleave="isDropdownHovered = false" style="">
-            <router-link to="/" class="block px-4 py-2 text-sm text-black hover:bg-red hover:text-white transition-opacity duration-300 ease-in-out" @mouseenter="setHoveredOnDropdown(true)" @mouseleave="setHoveredOnDropdown(false)"> Main Bridge</router-link>
-            <router-link to="/" class="block px-4 py-2 text-sm text-black hover:bg-red hover:text-white transition-opacity duration-300 ease-in-out" @mouseenter="setHoveredOnDropdown(true)" @mouseleave="setHoveredOnDropdown(false)"> CSA Library</router-link>
-            <router-link to="/" class="block px-4 py-2 text-sm text-black hover:bg-red hover:text-white transition-opacity duration-300 ease-in-out" @mouseenter="setHoveredOnDropdown(true)" @mouseleave="setHoveredOnDropdown(false)"> CSA Theater Library</router-link>
-            <router-link to="/" class="block px-4 py-2 text-sm text-black hover:bg-red hover:text-white transition-opacity duration-300 ease-in-out" @mouseenter="setHoveredOnDropdown(true)" @mouseleave="setHoveredOnDropdown(false)"> CSA Library Show Case</router-link>
-            <router-link to="/" class="block px-4 py-2 text-sm text-black hover:bg-red hover:text-white transition-opacity duration-300 ease-in-out" @mouseenter="setHoveredOnDropdown(true)" @mouseleave="setHoveredOnDropdown(false)"> Center of Technology (CoT)</router-link>
-            <router-link to="/" class="block px-4 py-2 text-sm text-black  hover:bg-red hover:text-white transition-opacity duration-300 ease-in-out" @mouseenter="setHoveredOnDropdown(true)" @mouseleave="setHoveredOnDropdown(false)"> CoT Ground Floor</router-link>
-      </div>
+  <div class="bg-navy h-[450px] flex mx-[100px] p-[20px] space-x-7">
+    <div>
+      <img class="h-20 w-auto mt-100" src="https://cot.unhas.ac.id/public/homepage/images/img/unhas.png">
+      <input class="text-black mb-[10px] mt-[10px] w-[400px] h-[35px] pl-5" type="text" placeholder="Search">
+      <div class="absolute">
+        <button @click="show = !show" class="flex items-center p-2 text-navy bg-white text-left w-[400px] h-[35px]">
+          <span class="mr-4 w-full whitespace-no-wrap">Faculty Of Engineering</span>
+          <svg class="w-5 h-5 text-navy dark:text-navy ml-[30px]" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"/></svg>
+        </button>
+        <div v-show="show" class="py-2 mt-2 bg-white rounded-0 shadow-xlw-44 ml-0 text-left" @mouseenter="isDropdownHovered = true" @mouseleave="isDropdownHovered = false" style="">
+          <router-link to="/" class="block px-4 py-2 text-sm text-black hover:bg-red hover:text-white transition-opacity duration-300 ease-in-out" @mouseenter="setHoveredOnDropdown(true)" @mouseleave="setHoveredOnDropdown(false)"> Main Bridge</router-link>
+          <router-link to="/" class="block px-4 py-2 text-sm text-black hover:bg-red hover:text-white transition-opacity duration-300 ease-in-out" @mouseenter="setHoveredOnDropdown(true)" @mouseleave="setHoveredOnDropdown(false)"> CSA Library</router-link>
+          <router-link to="/" class="block px-4 py-2 text-sm text-black hover:bg-red hover:text-white transition-opacity duration-300 ease-in-out" @mouseenter="setHoveredOnDropdown(true)" @mouseleave="setHoveredOnDropdown(false)"> CSA Theater Library</router-link>
+          <router-link to="/" class="block px-4 py-2 text-sm text-black hover:bg-red hover:text-white transition-opacity duration-300 ease-in-out" @mouseenter="setHoveredOnDropdown(true)" @mouseleave="setHoveredOnDropdown(false)"> CSA Library Show Case</router-link>
+          <router-link to="/" class="block px-4 py-2 text-sm text-black hover:bg-red hover:text-white transition-opacity duration-300 ease-in-out" @mouseenter="setHoveredOnDropdown(true)" @mouseleave="setHoveredOnDropdown(false)"> Center of Technology (CoT)</router-link>
+          <router-link to="/" class="block px-4 py-2 text-sm text-black  hover:bg-red hover:text-white transition-opacity duration-300 ease-in-out" @mouseenter="setHoveredOnDropdown(true)" @mouseleave="setHoveredOnDropdown(false)"> CoT Ground Floor</router-link>
+        </div>
       </div>
     </div>
-    <img class="position-absolute ml-[10px] p-[60px]" src="https://selatan.herald.id/wp-content/uploads/sites/10/2022/03/FT-Unhas.jpg" alt="">
+    <div class="flex items-center">
+      <img class="position-absolute" src="https://selatan.herald.id/wp-content/uploads/sites/10/2022/03/FT-Unhas.jpg" alt="">
     </div>
   </div>
 </template>
--- src/components/News.vue
+++ src/components/News.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <div class="space-y-5 mx-28 flex flex-col">
         <div class="flex flex-row space-x-10">
             <div class="basis-2/3 bg-navy font-Montserrat text-white font-medium text-center rounded text-3xl py-2">NEWS</div>
@@ -65,6 +65,201 @@
                 <div class="place-self-center">
                     <div class="border-2 border-red basis-1/3 w-40 flex justify-center">
                         <p class="text-red font-Inter font-semibold">All Information</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-row justify-between">
+            <div class="space-y-1 w-60">
+                <div>
+                    <img src="../assets/2.jpg" class="w-full">
+                </div>
+                <div class="flex items-center space-x-1">
+                    <div>
+                        <img src="../assets/calendar.png" width="12">
+                    </div>
+                    <div>
+                        <p class="text-gray2 font-Inter font-extralight text-[11px]">July 9th, 2019</p>
+                    </div>
+                    <div>
+                        <p class="text-black font-extrabold">|</p>
+                    </div>
+                    <div>
+                        <img src="../assets/admin.png" width="18">
+                    </div>
+                    <div>
+                        <p class="text-gray2 font-Inter font-extralight text-[11px]">Admin FT-UH</p>
+                    </div>
+                </div>
+                <div>
+                    <p class="font-Inter text-black text-md">Faculty of Engineering Hasanuddin University Celebrates 63rd Anniversary</p>
+                </div>
+                <div>
+                    <img src="../assets/readmore.png" width="100">
+                </div>
+            </div>
+            <div class="w-60 space-y-1">
+                <div>
+                    <img src="../assets/3.jpg" class="w-full">
+                </div>
+                <div class="flex items-center space-x-1">
+                    <div>
+                        <img src="../assets/calendar.png" width="12">
+                    </div>
+                    <div>
+                        <p class="text-gray2 font-Inter font-extralight text-[11px]">September 8th, 2023</p>
+                    </div>
+                    <div>
+                        <p class="text-black font-extrabold">|</p>
+                    </div>
+                    <div>
+                        <img src="../assets/admin.png" width="18">
+                    </div>
+                    <div>
+                        <p class="text-gray2 font-Inter font-extralight text-[11px]">Admin FT-UH</p>
+                    </div>
+                </div>
+                <div>
+                    <p class="font-Inter text-black text-md">Faculty of Engineering Establishes Collaboration with Electronics Engineering Polytechnic Institute of Surabaya</p>
+                </div>
+                <div>
+                    <img src="../assets/readmore.png" width="100">
+                </div>
+            </div>
+            <div class="w-60 space-y-1">
+                <div>
+                    <img src="../assets/4.jpg" class="w-full">
+                </div>
+                <div class="flex items-center space-x-1">
+                    <div>
+                        <img src="../assets/calendar.png" width="12">
+                    </div>
+                    <div>
+                        <p class="text-gray2 font-Inter font-extralight text-[11px]">September 4th, 2023</p>
+                    </div>
+                    <div>
+                        <p class="text-black font-extrabold">|</p>
+                    </div>
+                    <div>
+                        <img src="../assets/admin.png" width="18">
+                    </div>
+                    <div>
+                        <p class="text-gray2 font-Inter font-extralight text-[11px]">Admin FT-UH</p>
+                    </div>
+                </div>
+                <div>
+                    <p class="font-Inter text-black text-md">LPJK Holds Roadshow to Faculty of Engineering Campus</p>
+                </div>
+                <div>
+                    <img src="../assets/readmore.png" width="100">
+                </div>
+            </div>
+            <div class="space-y-1 w-60">
+                <div>
+                    <img src="../assets/5.jpg" class="w-full">
+                </div>
+                <div class="flex items-center space-x-1">
+                    <div>
+                        <img src="../assets/calendar.png" width="12">
+                    </div>
+                    <div>
+                        <p class="text-gray2 font-Inter font-extralight text-[11px]">September 20th, 2023</p>
+                    </div>
+                    <div>
+                        <p class="text-black font-extrabold">|</p>
+                    </div>
+                    <div>
+                        <img src="../assets/admin.png" width="18">
+                    </div>
+                    <div>
+                        <p class="text-gray2 font-Inter font-extralight text-[11px]">Admin FT-UH</p>
+                    </div>
+                </div>
+                <div>
+                    <p class="font-Inter text-black text-md">Increasing International Collaboration, Faculty of Engineering, Hasanuddin University Holds ICAST 2023</p>
+                </div>
+                <div>
+                    <img src="../assets/readmore.png" width="100">
+                </div>
+            </div>
+        </div>
+        <div class="place-self-center">
+            <div class="border-2 border-red basis-1/3 flex justify-center mt-8 w-44">
+                <p class="text-red font-Inter font-semibold">See More News</p>
+            </div>
+        </div>
+    </div>
+</template> -->
+
+<template>
+    <div class="space-y-5 mx-28 flex flex-col">
+        <div class="flex flex-row md:flex-col space-x-5">
+            <div class="flex flex-col basis-2/3 space-y-3">
+                <div class="bg-navy font-Montserrat text-white font-medium text-center rounded text-3xl py-2">NEWS</div>
+                <div class="rounded-sm relative">
+                    <img src="../assets/1.jpeg" class="rounded-md w-full">
+                    <span class=" absolute left-6 bottom-28">
+                        <img src="../assets/tanggal.png">
+                    </span>
+                    <span class="absolute left-6 right-24 bottom-4">
+                        <p class="font-Inter font-bold text-white text-justify text-shadow-sm shadow-black text-md">The Faculty of Engineering Hasanuddin University collaborate with the Indonesian Consultants Association or Perkindo by signing a Memorandum of Agreement on Monday, August 28th, 2023 in the CSA Building</p>
+                    </span>
+                    <span class="absolute right-2 bottom-3">
+                        <img src="../assets/readmore.png">
+                    </span>
+                </div>
+            </div>
+            <div class="flex flex-col basis-1/3 space-y-3">
+                <div class="bg-navy font-Montserrat text-white font-medium text-center rounded text-3xl py-2">INFORMATION</div>
+                <div class="border-2 border-navy rounded-sm space-y-6 p-5 grid justify-items-center">
+                    <div class="space-y-2">
+                        <p class="text-black font-Inter text-left font-light text-sm">THE 3RD EPI INTERNATIONAL CONFERENCE ON SCIENCE AND ENGINEERING 2019</p>
+                        <div class="flex items-center space-x-2">
+                            <div>
+                                <img src="../assets/calendar.png" width="14">
+                            </div>
+                            <div>
+                                <p class="text-gray2 font-Inter font-extralight text-xs">September 23rd, 2019</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="space-y-2">
+                        <p class="text-black font-Inter text-left font-light text-sm">WORKSHOP & SIMPOSIUM NASIONAL GEOMEKANIKA 5 2019</p>
+                        <div class="flex items-center space-x-2">
+                            <div>
+                                <img src="../assets/calendar.png" width="14">
+                            </div>
+                            <div>
+                                <p class="text-gray2 font-Inter font-extralight text-xs">August 6th, 2019</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="space-y-2">
+                        <p class="text-black font-Inter text-left font-light text-sm">UNHAS STUDENT LEADERSHIP ACADEMY (USLA) 2019</p>
+                        <div class="flex items-center space-x-2">
+                            <div>
+                                <img src="../assets/calendar.png" width="14">
+                            </div>
+                            <div>
+                                <p class="text-gray2 font-Inter font-extralight text-xs">July 9th, 2019</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="space-y-2">
+                        <p class="text-black font-Inter text-left font-light text-sm">INTERNATIONAL GUEST LECTURE ON JOURNAL ARTICLE</p>
+                        <div class="flex items-center space-x-2">
+                            <div>
+                                <img src="../assets/calendar.png" width="14">
+                            </div>
+                            <div>
+                                <p class="text-gray2 font-Inter font-extralight text-xs">Juny 25th, 2019</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pt-2">
+                        <div class="border-2 border-red basis-1/3 w-40 flex justify-center">
+                            <p class="text-red font-Inter font-semibold">All Information</p>
+                        </div>
                     </div>
                 </div>
             </div>
