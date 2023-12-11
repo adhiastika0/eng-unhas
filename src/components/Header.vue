7154d7260ab1e56e1f6ba1db26ffd22c66cf98e6
Justicly <adhiastika19@gmail.com>
2023-12-11 12:00:37 +0800

    Add dynamic route with slug params

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,81 @@
+<template>
+  <nav class="">
+    <div class="container min-w-full py-2 bg-navy">
+      <div class="container mx-auto">
+        <img
+          src="./assets/logo-teknik.png"
+          alt="Teknik Unhas"
+          class="mx-auto lg:mx-28"
+        />
+      </div>
+    </div>
+    <div
+      class="container min-w-full mx-auto bg-red font-Montserrat font-semibold text-sm"
+    >
+      <div class="flex flex-row justify-center flex-wrap text-white px-10">
+        <!-- Loop untuk membuat dropdown -->
+        <div
+          v-for="(item, index) in navbarItems"
+          :key="index"
+          class="relative z-50"
+          id="dropdownButton"
+        >
+          <div
+            @mouseover="toggleDropdown(index)"
+            @mouseout="addDropdown(index)"
+            class="hover:bg-white hover:text-red"
+          >
+            <div class="py-2 px-4">
+              <router-link :to="'/'">{{ item.title }}</router-link>
+            </div>
+
+            <div class="absolute">
+              <div :id="'dropdown' + index" class="absolute hidden">
+                <div
+                  @click="toggleDropdown(index)"
+                  v-for="(dropdownItem, dropdownIndex) in item.children"
+                  :key="dropdownIndex"
+                  class="w-40 py-2 px-5 bg-white border-gray shadow-md text-red cursor-pointer hover:bg-red hover:text-white hover:border-white"
+                >
+                  <router-link :to="'/pages/' + dropdownItem.slug">{{
+                    dropdownItem.title
+                  }}</router-link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container mx-auto relative min-w-full z-0">
+      <img
+        src="./assets/jumbotron.png"
+        alt="Faculty of Engineering"
+        class="opacity-80 object-cover min-w-full max-h-[500px] z-0"
+      />
+      <div class="absolute mx-auto left-0 right-0 max-w-7xl">
+        <div
+          class="absolute bg-gray text-white py-2 px-2 mx-auto sm:left-28 sm:text-md sm:bottom-32 lg:text-3xl lg:bottom-36 font-extrabold font-Montserrat"
+        >
+          WELCOME TO
+        </div>
+        <div
+          class="absolute bg-navy text-white py-2 px-2 sm:left-28 sm:text-md sm:bottom-20 lg:text-3xl font-extrabold font-Montserrat"
+        >
+          FACULTY OF ENGINEERING, HASANUDDIN UNIVERSITY
+        </div>
+      </div>
+    </div>
+  </nav>
+  <router-view/>
+</template>
+
+
 <script>
 import { onMounted, ref } from 'vue';
 
 export default {
-  name: 'Header',
+name: 'Header',
   props: {
     msg: String,
   },
@@ -76,73 +149,3 @@ export default {
 };
 </script>
 
-<template>
-  <nav class="">
-    <div class="container min-w-full py-2 bg-navy">
-      <div class="container mx-auto">
-        <img
-          src="../assets/logo-teknik.png"
-          alt="Teknik Unhas"
-          class="mx-auto lg:mx-28"
-        />
-      </div>
-    </div>
-    <div
-      class="container min-w-full mx-auto bg-red font-Montserrat font-semibold text-sm"
-    >
-      <div class="flex flex-row justify-center flex-wrap text-white px-10">
-        <!-- Loop untuk membuat dropdown -->
-        <div
-          v-for="(item, index) in navbarItems"
-          :key="index"
-          class="relative z-50"
-          id="dropdownButton"
-        >
-          <div
-            @mouseover="toggleDropdown(index)"
-            @mouseout="addDropdown(index)"
-            class="hover:bg-white hover:text-red"
-          >
-            <div class="py-2 px-4">
-              <router-link :to="'/'">{{ item.title }}</router-link>
-            </div>
-
-            <div class="absolute">
-              <div :id="'dropdown' + index" class="absolute hidden">
-                <div
-                  @click="toggleDropdown(index)"
-                  v-for="(dropdownItem, dropdownIndex) in item.children"
-                  :key="dropdownIndex"
-                  class="w-40 py-2 px-5 bg-white border-gray shadow-md text-red cursor-pointer hover:bg-red hover:text-white hover:border-white"
-                >
-                  <router-link :to="'/pages/' + dropdownItem.slug">{{
-                    dropdownItem.title
-                  }}</router-link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="container mx-auto relative min-w-full z-0">
-      <img
-        src="../assets/jumbotron.png"
-        alt="Faculty of Engineering"
-        class="opacity-80 object-cover min-w-full max-h-[500px] z-0"
-      />
-      <div class="absolute mx-auto left-0 right-0 max-w-7xl">
-        <div
-          class="absolute bg-gray text-white py-2 px-2 mx-auto sm:left-28 sm:text-md sm:bottom-32 lg:text-3xl lg:bottom-36 font-extrabold font-Montserrat"
-        >
-          WELCOME TO
-        </div>
-        <div
-          class="absolute bg-navy text-white py-2 px-2 sm:left-28 sm:text-md sm:bottom-20 lg:text-3xl font-extrabold font-Montserrat"
-        >
-          FACULTY OF ENGINEERING, HASANUDDIN UNIVERSITY
-        </div>
-      </div>
-    </div>
-  </nav>
-</template>
--- src/views/SlugView.vue
+++ src/views/SlugView.vue
@@ -1,18 +1,79 @@
 <template>
   <div class="slug">
     <h1>This is an about page{{ $route.params.slugParam }}</h1>
+    <div v-for="(item, index) in pageItems" :key="index">
+      <div>{{ item.items.headline }}</div>
+      <div v-html="item.items.content"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import { useRouter } from 'vue-router';
+import { useRouter, useRoute } from 'vue-router';
+import { onMounted, ref, watch } from 'vue';
 
 export default {
   setup() {
+    const pageItems = ref([]);
     const router = useRouter();
+    const route = useRoute();
+
+    const getPages = async () => {
+      try {
+        const response = await fetch(
+          `https://directus-npm-test-production.up.railway.app/items/pages?filter[slug]=${route.params.slugParam}&fields[]=data&fields[]=blocks.item&fields[]=blocks.item.content&fields[]=blocks.item.headline`
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const responseData = await response.json();
+        console.log(responseData);
+
+        // Check if the 'data' property exists in the response
+        if (Object.prototype.hasOwnProperty.call(responseData, 'data')) {
+          const data = responseData.data;
+
+          // Mengubah nilai pages dengan nilai yang baru
+          pageItems.value = data.flatMap((data) =>
+            data.blocks.map((block) => ({
+              items: block.item,
+            }))
+          );
+
+          console.log(pageItems.value);
+
+          // Mengembalikan pageItems sebagai variabel yang dapat diakses di komponen lain
+          return { pageItems };
+        } else {
+          console.error('Invalid API response:', responseData);
+          // Handle this case according to your requirements
+          return { pageItems: [] };
+        }
+      } catch (error) {
+        console.error('Fetch error:', error);
+        // Handle the error gracefully (e.g., show a message to the user)
+        throw error; // Re-throw the error to handle it at the component level if needed
+      }
+    };
+    onMounted(async () => {
+      // Panggil getPages untuk menginisialisasi pageItems saat komponen di-mount
+      await getPages();
+    });
+
+    watch(
+      () => route.params.slugParam,
+      async (newSlug, oldSlug) => {
+        // Jika nilai slug berubah, panggil getPages dan refresh halaman
+        if (newSlug !== oldSlug) {
+          await getPages();
+        }
+      }
+    );
 
     return {
       router,
+      pageItems,
     };
   },
 };
